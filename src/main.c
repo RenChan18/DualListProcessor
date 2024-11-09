fdef7a8549a7955d5f7f2e8d6636bb09b0368677
RenChan18 <renya.talacheva@gmail.com>
2024-11-09 22:27:49 +0300

    tested bidirlist

--- src/main.c
+++ src/main.c
@@ -1,16 +1,42 @@
 // main.c
 #include <stdio.h>
+#include <time.h>
 #include "list.h"
 
-int main() {
+
+#ifndef RAND_RANGE
+#define RAND_RANGE 100
+#endif
+
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE 10
+#endif
+
+void random_fill(int *arr, size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        arr[i] = rand() % RAND_RANGE;
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    int *arr = malloc(ARRAY_SIZE * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    srand(time(NULL));
+    random_fill(arr, ARRAY_SIZE);
+    
     List *list = createList();
     if (list == NULL) {
         fprintf(stderr, "Failed to create list\n");
         return 1;
     }
 
-    for (int i = 1; i <= 5; i++) {
-        pushBack(list, i);
+    for (int i = 0; i <= ARRAY_SIZE; i++) {
+        pushBack(list, arr[i]);
         
     }
 
